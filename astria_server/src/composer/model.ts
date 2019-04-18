1145a9324c37068be4b6093cfb1837ad08a431f8
Arpit007 <arpit2011@live.com>
2019-04-18 10:02:13 +0530

    Added New Models to Astria Server

--- astria_server/src/composer/model.ts
+++ astria_server/src/composer/model.ts
@@ -18,50 +18,84 @@ export class Election {
     public electionName: string;
     public startDate: Date;
     public endDate: Date;
+    public idKey: string;
+    public voteEncKey: string;
+    public voteDecKey: string;
+    public freeze: boolean;
     public adminId: string;
+    public managers: string[];
     
-    constructor(electionId: string, electionName: string, startDate: Date, endDate: Date, adminId: string) {
+    constructor(electionId: string, electionName: string, startDate: Date, endDate: Date, idKey: string, voteEncKey: string, voteDecKey: string, freeze: boolean, adminId: string, managers: string[]) {
         this.electionId = electionId;
         this.electionName = electionName;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.idKey = idKey;
+        this.voteEncKey = voteEncKey;
+        this.voteDecKey = voteDecKey;
+        this.freeze = freeze;
         this.adminId = adminId;
+        this.managers = managers;
     }
 }
+
 
 export class AstriaAdmin {
     public userId: string;
-    public electionId: string;
-    public voteKey: string;
-    public voteDecKey: string;
-    public idKey: string;
-    public email: string;
-    public secret: string;
     
-    
-    constructor(userId: string, electionId: string, voteKey: string, voteDecKey: string,
-                idKey: string, email: string, secret: string) {
+    constructor(userId: string) {
         this.userId = userId;
-        this.electionId = electionId;
-        this.voteKey = voteKey;
-        this.voteDecKey = voteDecKey;
-        this.idKey = idKey;
-        this.email = email;
-        this.secret = secret;
     }
 }
 
-export class Manager {
-    public userId: string;
-    public email: string;
-    public loginId: string;
-    public secret: string;
+
+export class Vote {
+    public voteId: string;
+    public electionId: string;
+    public candidateId: string;
+    public hasVoted: boolean;
     
+    constructor(voteId: string, electionId: string, candidateId: string, hasVoted: boolean) {
+        this.voteId = voteId;
+        this.electionId = electionId;
+        this.candidateId = candidateId;
+        this.hasVoted = hasVoted;
+    }
+}
+
+
+export class CandidateResult {
+    public candidateId: string;
+    public voteCount: number;
     
-    constructor(userId: string, email: string, loginId: string, secret: string) {
-        this.userId = userId;
-        this.email = email;
-        this.loginId = loginId;
-        this.secret = secret;
+    constructor(candidateId: string, voteCount: number) {
+        this.candidateId = candidateId;
+        this.voteCount = voteCount;
+    }
+}
+
+
+export class Result {
+    public resultId: string;
+    public electionId: string;
+    public results: CandidateResult[];
+    
+    constructor(resultId: string, electionId: string, results: CandidateResult[]) {
+        this.resultId = resultId;
+        this.electionId = electionId;
+        this.results = results;
+    }
+}
+
+
+export class ResultVote {
+    public voteId: string;
+    public electionId: string;
+    public candidateId: string;
+    
+    constructor(voteId: string, electionId: string, candidateId: string) {
+        this.voteId = voteId;
+        this.electionId = electionId;
+        this.candidateId = candidateId;
     }
 }
